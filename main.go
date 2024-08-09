0727474250d88872eae6c83f4ee7b9f8585f76d4
anousone <anousone.litd@gmail.com>
2024-08-09 16:27:40 +0700

    refactor code by create v1, v2

--- main.go
+++ main.go
@@ -1,139 +1,24 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
+	"register/v1/user"
+	user2 "register/v2/user"
 
 	"github.com/labstack/echo/v4"
 )
 
-// database
-var users []*User
-
-var id int
-
-type User struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Gender    string `json:"gender"`
-	Phone     string `json:"phone"`
-	Address   string `json:"address"`
-}
-
-func initUsers() {
-	id++
-	item := User{
-		ID:        id,
-		FirstName: "jojo",
-		LastName:  "jj",
-		Gender:    "male",
-		Phone:     "+8562094938282",
-		Address:   "ນາຄຳ",
-	}
-	users = append(users, &item)
-
-	id++
-	item2 := User{
-		ID:        id,
-		FirstName: "sone",
-		LastName:  "freestyle",
-		Gender:    "male",
-		Phone:     "+8562094938283",
-		Address:   "ສາລາຄຳ",
-	}
-	users = append(users, &item2)
-}
-
-func listUsers(c echo.Context) error {
-	return c.JSON(200, users)
-}
-
-func createUser(c echo.Context) error {
-	var req User
-
-	if err := c.Bind(&req); err != nil {
-		return err
-	}
-
-	id++
-	req.ID = id
-	users = append(users, &req)
-
-	return c.JSON(http.StatusOK, echo.Map{
-		"message": "success",
-	})
-}
-
-func updateUser(c echo.Context) error {
-	var req User
-	if err := c.Bind(&req); err != nil {
-		// customer error
-		return err
-	}
-
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		// customer error
-		return err
-	}
-
-	for _, i := range users {
-		if i.ID == id {
-			i.FirstName = req.FirstName
-			i.LastName = req.LastName
-			i.Gender = req.Gender
-			i.Phone = req.Phone
-			i.Address = req.Address
-		}
-	}
-	return c.JSON(http.StatusOK, echo.Map{
-		"message": "success",
-	})
-}
-func deleteUser(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		// customer error
-		return err
-	}
-
-	//id: 4
-	// 1, 2, 3, 4, 5, 6, 7
-	// users[:id] = 1, 2, 3
-	//users[id+1:] = 5, 6, 7
-
-	isFound := false
-	for index, i := range users {
-		if i.ID == id {
-			isFound = true
-			users = append(users[:index], users[index+1:]...)
-		}
-	}
-
-	if !isFound {
-		return c.JSON(http.StatusOK, echo.Map{
-			"message": "not found",
-		})
-	}
-
-	return c.JSON(http.StatusOK, echo.Map{
-		"message": "success",
-	})
-
-}
-
 func main() {
 	e := echo.New()
-	initUsers()
+	user.InitUsers()
 
-	e.GET("/users", listUsers)
+	v1 := e.Group("/v1")
+	v1.GET("/users", user.ListUsers)
+	v1.POST("/users", user.CreateUser)
+	v1.PUT("/users/:id", user.UpdateUser)
+	v1.DELETE("/users/:id", user.DeleteUser)
 
-	e.POST("/users", createUser)
-
-	e.PUT("/users/:id", updateUser)
-
-	e.DELETE("/users/:id", deleteUser)
+	v2 := e.Group("/v2")
+	v2.GET("/users", user2.ListUsers)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
---
+++ v2/user/user.go
@@ -0,0 +1,123 @@
+package user
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+// database
+var users []*User
+
+var id int
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Gender    string `json:"gender"`
+	Phone     string `json:"phone"`
+	Address   string `json:"address"`
+}
+
+func InitUsers() {
+	id++
+	item := User{
+		ID:        id,
+		FirstName: "jojo",
+		LastName:  "jj",
+		Gender:    "male",
+		Phone:     "+8562094938282",
+		Address:   "ນາຄຳ",
+	}
+	users = append(users, &item)
+
+	id++
+	item2 := User{
+		ID:        id,
+		FirstName: "sone",
+		LastName:  "freestyle",
+		Gender:    "male",
+		Phone:     "+8562094938283",
+		Address:   "ສາລາຄຳ",
+	}
+	users = append(users, &item2)
+}
+
+func ListUsers(c echo.Context) error {
+	return c.JSON(200, users)
+}
+
+func CreateUser(c echo.Context) error {
+	var req User
+
+	if err := c.Bind(&req); err != nil {
+		return err
+	}
+
+	id++
+	req.ID = id
+	users = append(users, &req)
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "success",
+	})
+}
+
+func UpdateUser(c echo.Context) error {
+	var req User
+	if err := c.Bind(&req); err != nil {
+		// customer error
+		return err
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// customer error
+		return err
+	}
+
+	for _, i := range users {
+		if i.ID == id {
+			i.FirstName = req.FirstName
+			i.LastName = req.LastName
+			i.Gender = req.Gender
+			i.Phone = req.Phone
+			i.Address = req.Address
+		}
+	}
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "success",
+	})
+}
+func DeleteUser(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// customer error
+		return err
+	}
+
+	//id: 4
+	// 1, 2, 3, 4, 5, 6, 7
+	// users[:id] = 1, 2, 3
+	//users[id+1:] = 5, 6, 7
+
+	isFound := false
+	for index, i := range users {
+		if i.ID == id {
+			isFound = true
+			users = append(users[:index], users[index+1:]...)
+		}
+	}
+
+	if !isFound {
+		return c.JSON(http.StatusOK, echo.Map{
+			"message": "not found",
+		})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "success",
+	})
+}
